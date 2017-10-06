4321ac5480b52bb3bbcb3c92bd49a259edcb2b8d
ayseelifyoldas <elif.yoldas@tubitak.gov.tr>
2017-10-06 10:50:16 +0300

    branch class is changed

--- com.vogella.osgi.firstbundle.internal/src/com/vogella/osgi/firstbundle/internal/Branch.java
+++ com.vogella.osgi.firstbundle.internal/src/com/vogella/osgi/firstbundle/internal/Branch.java
@@ -1,5 +1,13 @@
 package com.vogella.osgi.firstbundle.internal;
 
 public class Branch {
+		private int change;
 
+		public int getChange() {
+			return change;
+		}
+
+		public void setChange(int change) {
+			this.change = change;
+		}
 }
